bcb582c639036ebc1172354f2a4acef8cff2435b
Radu Balanescu <44756416+p051xn1nja@users.noreply.github.com>
2026-02-13 17:00:46 -0500

    Simplify detail view to single progress control

--- index.php
+++ index.php
@@ -536,17 +536,13 @@ $csrfToken = $_SESSION['csrf_token'];
                       <p class="desc"><?= htmlspecialchars((string) $task['description'], ENT_QUOTES, 'UTF-8'); ?></p>
                     <?php endif; ?>
 
-                    <div class="progress-wrap">
-                      <progress class="js-progress-bar" max="100" value="<?= (int) ($task['progress'] ?? 0); ?>"></progress>
-                      <strong class="js-progress-value"><?= (int) ($task['progress'] ?? 0); ?>%</strong>
-                    </div>
-
                     <form class="slider-form" method="post">
                       <input type="hidden" name="action" value="updateProgress">
                       <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrfToken, ENT_QUOTES, 'UTF-8'); ?>">
                       <input type="hidden" name="id" value="<?= htmlspecialchars((string) ($task['id'] ?? ''), ENT_QUOTES, 'UTF-8'); ?>">
                       <?php if ($searchQuery !== ''): ?><input type="hidden" name="q" value="<?= htmlspecialchars($searchQuery, ENT_QUOTES, 'UTF-8'); ?>"><?php endif; ?>
                       <input class="js-progress-slider" type="range" name="progress" min="0" max="100" step="1" value="<?= (int) ($task['progress'] ?? 0); ?>">
+                      <strong class="js-progress-value"><?= (int) ($task['progress'] ?? 0); ?>%</strong>
                       <button class="ghost-btn" type="submit">Set progress</button>
                     </form>
                   </div>
